0e93a2a2c7fe5263376d3676230cfaabc79ce13e
Johanna Liu <johanna.liu@mths.ca>
2022-09-19 15:19:58 +0000

    added code

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Johanna liu
+# Created on: Sep 2022
+# This program calculates the area and perimeter of a rectangle
+#    with dimensions 5cm x 4cm
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("If a rectangle has the dimensions: ")
+    print("5cm x 4cm")
+    print("")
+    print("Area is {}cm².".format(4 * 5))
+    print("Perimeter is {}cm.".format(2 * (4 + 5)))
+
+
+if __name__ == "__main__":
+    main()
